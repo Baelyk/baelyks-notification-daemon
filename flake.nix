9a9b67ffd81ec5466aa8d78f16c9b8c1f2ece964
Baelyk <b43lyk@gmail.com>
2025-06-20 11:42:18 +0200

    Rewrite with Iced

--- flake.nix
+++ flake.nix
@@ -6,57 +6,82 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, fenix, nixpkgs, flake-utils}:
-  let
-      name = "baelyks-notification-daemon";
-      displayname = "Baelyk's notification daemon";
+  outputs = {
+    self,
+    fenix,
+    nixpkgs,
+    flake-utils,
+  }: let
+    name = "baelyks-notification-daemon";
+    displayname = "Baelyk's notification daemon";
   in
-    flake-utils.lib.eachDefaultSystem(system:
-    let
+    flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       toolchain = fenix.packages.${system}.stable.toolchain;
     in {
-      packages.default = (pkgs.makeRustPlatform {
-        cargo = toolchain;
-        rustc = toolchain;
-      }).buildRustPackage {
-        pname = name;
-        version = "0.1.0";
+      packages.default =
+        (pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        })
+        .buildRustPackage {
+          pname = name;
+          version = "0.2.0";
 
-        src = ./.;
+          src = ./.;
 
-        cargoLock.lockFile = ./Cargo.lock;
+          cargoLock.lockFile = ./Cargo.lock;
 
-        # DBUS Service file
-        postInstall = ''
-          mkdir -p $out/share/dbus-1/services
-          cat <<END > $out/share/dbus-1/services/org.baelyk.${name}.service
-          [D-BUS Service]
-          Name=org.freedesktop.Notifications
-          Exec=$out/bin/${name}
-          SystemdService=${name}.service
-        '';
+          # DBUS Service file
+          postInstall = ''
+            mkdir -p $out/share/dbus-1/services
+            cat <<END > $out/share/dbus-1/services/org.baelyk.${name}.service
+            [D-BUS Service]
+            Name=org.freedesktop.Notifications
+            Exec=$out/bin/${name}
+            SystemdService=${name}.service
+          '';
+        };
 
-      };
+      devShells.default = pkgs.mkShell rec {
+        # For Iced https://github.com/iced-rs/iced/blob/master/DEPENDENCIES.md
+        buildInputs = with pkgs; [
+          expat
+          fontconfig
+          freetype
+          freetype.dev
+          libGL
+          pkg-config
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          wayland
+          libxkbcommon
+        ];
+        LD_LIBRARY_PATH = builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" buildInputs;
 
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
+        packages = [
           toolchain
         ];
 
         shellHook = ''
-            echo $(cargo --version)
+          echo $(cargo --version)
 
-            exec fish
+          exec fish
         '';
       };
     })
     // flake-utils.lib.eachDefaultSystemPassThrough (system: {
-      nixosModules.default = { config, lib, ... }: let
+      nixosModules.default = {
+        config,
+        lib,
+        ...
+      }: let
         cfg = config.services.${name};
       in {
         options = {
@@ -72,8 +97,8 @@
           };
         };
 
-        config = lib.mkIf cfg.enable ({
-          home.packages = [ cfg.package ];
+        config = lib.mkIf cfg.enable {
+          home.packages = [cfg.package];
 
           systemd.user.services.${name} = {
             Unit = {
@@ -88,8 +113,7 @@
               ExecStart = "${cfg.package}/bin/${name}";
             };
           };
-        });
+        };
       };
     });
 }
-
